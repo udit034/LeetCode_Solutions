d043f29e9ee41558405203a97c13e251835f562f
udit034 <udit.mehata@gmail.com>
2018-06-07 22:00:01 -0700

    Adding a new competitive coding submission problem solution

---
+++ Solution.AddDigits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        while( num >= 10 )
+        {
+            int temp = 0;
+            while( num != 0 )
+            {
+                temp += num % 10;
+                num = num / 10;
+            }
+            num = temp;
+        }
+        return num;
+    }
+}
