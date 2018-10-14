5207c4d13ff0df93c80a9fa87d427720c6073ddb
udit034 <udit.mehata@gmail.com>
2018-10-13 23:00:02 -0700

    Adding a new competitive coding submission problem solution

---
+++ Solution.Kth Smallest Element in a BST.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+      public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+        }
+        System.out.println( k  + " " + count );
+        return root.val;
+    }
+    
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;
+        
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+}
